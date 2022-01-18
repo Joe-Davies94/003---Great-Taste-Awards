a634cc1a197867b78f29b2a559c9b6fe0f48042f
Joe Davies <j.adam.davies1994@gmail.com>
2022-01-18 17:59:38 +0000

    Updated year input. Added date and time. Updated comments for clarity.

--- Welsh Great Taste Awards.py
+++ Welsh Great Taste Awards.py
@@ -1,31 +1,37 @@
+# Identify and report the number of Welsh Great Taste Awards
+
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 from datetime import date
 
+# Set variable to current date
+today=date.today()
+today=today.strftime("%Y-%m-%d")
+
+# Create empty dataframe
 df=pd.DataFrame({
 })
 
-""" Commented out - not working yet
-
+# Input for either 2020 or 2021. 2019 and before doesn't work due to different url structure
 print("Enter year 2020 or 2021: ")
 year = input()
-if year == 2020:
-    year=2020
+if year == str(2020):
     years=2020
 else:
     year=""
     years=2021
+print(years)
 
-"""
 
-
-def countFunction(county_id):
+# Function for identifying ratings
+def countFunction(county_id, name):
     global df
+    global year
 
     # gets the html of each Welsh county
     url = requests.get(
-        "https://greattasteawards.co.uk/results?_token=8c3gzxgcKmBFflUOBlAITyxSMrHPeVhdRdcofLli&sort_options_limit=50&sort_options_sort_direction=desc&sort_options_sort_by=rating&Keyword=&mg_rating_id=&mg_category_id=&country_id=&county_id="+str(county_id)+"&filter=Search"
+        "https://greattasteawards.co.uk/results"+str(year)+"?_token=8c3gzxgcKmBFflUOBlAITyxSMrHPeVhdRdcofLli&sort_options_limit=50&sort_options_sort_direction=desc&sort_options_sort_by=rating&Keyword=&mg_rating_id=&mg_category_id=&country_id=&county_id="+str(county_id)+"&filter=Search"
         )
     bsurl = BeautifulSoup(url.text, "html.parser")
     
@@ -42,32 +48,32 @@ def countFunction(county_id):
     "2-Star Awards":[ratings2],
     "1-Star Awards":[ratings1],
     })
-    df2.index = [str(county_id)]
+    df2.index = [str(name)]
 
     df = df.append(df2)
     return df
 
 # Run the above function for each Welsh county
-countFunction("116")
-countFunction("117")
-countFunction("118")
-countFunction("119")
-countFunction("120")
-countFunction("121")
-countFunction("122")
-countFunction("123")
-countFunction("124")
-countFunction("125")
-countFunction("126")
-countFunction("127")
-countFunction("128")
-countFunction("129")
-countFunction("130")
-countFunction("131")
+countFunction("116", "Carmarthenshire")
+countFunction("117", "Ceredigon")
+countFunction("118", "Clwyd")
+countFunction("119", "Denbighshire")
+countFunction("120", "Flintshire")
+countFunction("121", "Glamorgan")
+countFunction("122", "Gwent")
+countFunction("123", "Gwynedd")
+countFunction("124", "Anglesey")
+countFunction("125", "Mid-Glamorgan")
+countFunction("126", "Monmouthshire")
+countFunction("127", "Pembrokeshire")
+countFunction("128", "Powys")
+countFunction("129", "Rhondda")
+countFunction("130", "South Glamorgan")
+countFunction("131", "West Glamorgan")
 
+# Add total row to data frame and export to excel
 df.loc["Total"] = df.sum()
-
-df.to_excel("Great Taste Awards.xlsx")
+df.to_excel(today+" - Great Taste Awards "+str(years)+".xlsx")
 
 print("Complete")
 
